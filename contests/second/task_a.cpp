53456daf22c1a8b8d2b399d2e82a9159f64bdf5f
Ekaterina Selyankina <selyankina@ispras.ru>
2023-09-20 17:34:08 +0300

    update task_a to google style guide

--- contests/second/task_a.cpp
+++ contests/second/task_a.cpp
@@ -1,79 +1,72 @@
-
+// Google style code
 #include <iostream>
 #include <vector>
 
-class TGraph {
-private:
-    std::vector<std::vector<int>> adjacency_matrix_;
-    uint32_t count_vertex_;
+class Graph {
 public:
-    explicit TGraph(int n) {
-        count_vertex_ = n;
-        adjacency_matrix_.resize(count_vertex_);
-        for(uint32_t i=0; i<count_vertex_; i++) {
-            adjacency_matrix_[i].resize(count_vertex_);
-            for(uint32_t j=0; j<count_vertex_; j++) {
-                adjacency_matrix_[i][j] = 0;
-            }
-        }
+  explicit Graph(int n) {
+    count_vertex_ = n;
+    adjacency_matrix_.resize(count_vertex_);
+    for(uint32_t i=0; i<count_vertex_; i++) {
+      adjacency_matrix_[i].resize(count_vertex_);
+      for(uint32_t j=0; j<count_vertex_; j++) {
+        adjacency_matrix_[i][j] = 0;
+      }
+    }
+  }
+
+  ~Graph() {
+
+  }
+
+  void ReadFromStream(std::istream& stream) {
+    for(uint32_t i=0; i<count_vertex_; i++) {
+      for (uint32_t j=0; j<count_vertex_; j++) {
+        stream >> adjacency_matrix_[i][j];
+      }
+    }
+    return;
+  }
+
+  int GetConnectedComponent(const uint32_t vertex) const {
+    std::vector<int> visited(count_vertex_);
+    for(uint32_t i=0; i<count_vertex_; i++) {
+      visited[i] = 0;
+    }
+    visited[vertex-1] = 1;
+
+    for(uint32_t i=0; i<count_vertex_; i++) {
+      if (adjacency_matrix_[vertex-1][i] == 1) {
+        DepthFirstSearch(i, visited);
+      }
     }
 
-    void ReadFromStream(std::istream& stream) {
-        for(uint32_t i=0; i<count_vertex_; i++) {
-            for (uint32_t j=0; j<count_vertex_; j++) {
-                stream >> adjacency_matrix_[i][j];
-            }
-        }
-        return;
+    uint32_t result = 0;
+    for(uint32_t i=0; i<count_vertex_; i++) {
+      result += visited[i];
     }
-
-    void output_data() const {
-        for(int i=0; i<count_vertex_; i++) {
-            for (int j=0; j<count_vertex_; j++) {
-                std::cout << adjacency_matrix_[i][j];
-            }
-            std::cout << '\n';
-        }
-        return;
-    }
-
-    int GetConnectedComponent(const uint32_t vertex) const {
-        std::vector<int> visited(count_vertex_);
-        for(uint32_t i=0; i<count_vertex_; i++) {
-            visited[i] = 0;
-        }
-        visited[vertex-1] = 1;
-
-        for(uint32_t i=0; i<count_vertex_; i++) {
-            if (adjacency_matrix_[vertex-1][i] == 1) {
-                DepthFirstSearch(i, visited);
-            } 
-        }
-
-        uint32_t result = 0;
-        for(uint32_t i=0; i<count_vertex_; i++) {
-            result += visited[i];
-        }
-        return result;
-    }
+    return result;
+  }
 
 private:
-    void DepthFirstSearch(const uint32_t vertex, std::vector<int> &visited) const {
-        visited[vertex] = 1;
-        for(uint32_t i=0; i<count_vertex_; i++) {
-            if (visited[i] != 1 and adjacency_matrix_[vertex][i] == 1) {
-                DepthFirstSearch(i, visited);
-            }
-        }
+  void DepthFirstSearch(const uint32_t vertex, std::vector<int> &visited) const {
+    visited[vertex] = 1;
+    for(uint32_t i=0; i<count_vertex_; i++) {
+      if (visited[i] != 1 and adjacency_matrix_[vertex][i] == 1) {
+        DepthFirstSearch(i, visited);
+      }
     }
+  }
+
+  std::vector<std::vector<int>> adjacency_matrix_;
+  uint32_t count_vertex_;
 };
 
 int main() {
-    int N, S;
-    std::cin >> N >> S;
-    TGraph test(N);
-    test.ReadFromStream(std::cin);
-    // test.output_data();
-    std::cout << test.GetConnectedComponent(S);
+    int n, s;
+    std::cin >> n >> s;
+    Graph graph(n);
+    graph.ReadFromStream(std::cin);
+    std::cout << graph.GetConnectedComponent(s);
     return 0;
 }
